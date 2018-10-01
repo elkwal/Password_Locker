7b5118e5b7779895d007debccf7a016266b5b0cf
Elukwal <elukwal3@gmail.com>
2018-10-01 06:19:19 +0300

    add the function test_save_user to check if the new users information is saved

--- user_test.py
+++ user_test.py
@@ -19,3 +19,9 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.first_name, "Sylvia")
         self.assertEqual(self.new_user.last_name, "Elkwal")
         self.assertEqual(self.new_user.password, "Elukwal3")
+    def test_save_user(self):
+        """
+		Test to check if the new users information is saved into the users list
+		"""
+        self.new_user.save_user()
+        self.assertEqual(len(User.users_list), 1)
