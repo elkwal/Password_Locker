5c4e86e51df4ff019d2398e620a2da8a3c477ae8
Elukwal <elukwal3@gmail.com>
2018-10-01 06:24:45 +0300

    add the instance variables

--- user.py
+++ user.py
@@ -5,3 +5,49 @@ class User:
     """
     Class that generates the user's information
     """
+
+
+    def __init__(self, first_name, last_name, password):
+
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+
+    users_list = [] #Empty users list
+
+    def save_user(self):
+        """
+		This function helps in saving the created property with the password
+		"""
+        User.users_list.append(self)
+
+class Credential:
+    """
+	Class that creates  account credentials, generate passwords and save their information
+	"""
+
+    credentials_list = []
+
+    @classmethod
+    def check_existing_user(cls, first_name, password):
+        """
+		Method that checks if the name and password entered match entries in the users_list
+		"""
+        current_user = ''
+        for user in User.users_list:
+            if (user.first_name == first_name and user.password == password):
+                current_user = user.first_name
+        return current_user
+
+    def __init__(self, user_name, site_name, account_name, password):
+        '''
+		Method to define the properties for each user object will hold.
+		'''
+
+        # instance variables
+
+        self.user_name = user_name
+        self.site_name = site_name
+        self.account_name = account_name
+        self.password = password
