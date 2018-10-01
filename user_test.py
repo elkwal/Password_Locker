2770d018ce6bc81f6df1447bfa8340e45130ad25
Elukwal <elukwal3@gmail.com>
2018-10-01 06:15:28 +0300

    add the sub class TestUser

--- user_test.py
+++ user_test.py
@@ -1,3 +1,8 @@
 import unittest
 import pyperclip
 from user import User,Credential
+
+class TestUser(unittest.TestCase):
+    """
+	Test class which defines test cases for the user class behaviours
+	"""
